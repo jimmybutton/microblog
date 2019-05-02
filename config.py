d003a33aa42b5e60414ef666ac09fffa1e38d5f2
sliedtkegranta <samuel.liedtke@grantadesign.com>
2019-05-02 17:54:51 +0100

    chapter database completed

--- config.py
+++ config.py
@@ -1,4 +1,8 @@
 import os
+basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'ka8:4hQY3r3,dv80>9234lgD'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+        'sqlite:///' + os.path.join(basedir, 'app.db')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
